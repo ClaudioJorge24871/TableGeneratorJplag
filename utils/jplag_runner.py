f31ba1f8d45da53e73ce32e79e6012b119f33814
owanedee <claudiojorge9090@gmail.com>
2024-11-24 14:58:47 +0100

    Created gitignore, jplagrunner configuration

---
+++ utils/jplag_runner.py
@@ -0,0 +1,36 @@
+import subprocess
+
+def run_jplag(jplag_path, dir_source, dir_output):
+    """
+    Runs jplag 
+    given the path of jplag, the source/data and the directory of output 
+    
+    PARAMS:
+    jplag_path - path to the jplag jar executable
+    dir_source - directory to the code submissions
+    dir_output - directory to the code results  
+    """
+
+    # Language required by jplag, must be the language of the submitted codes 
+    language = "java"
+
+    # Creates the prompt to be executed to run jplag
+    prompt = [
+        "java","-jar",jplag_path,
+        "-l",language,
+        dir_source,
+        "-r",dir_output
+    ]
+
+    # Try-Catch to run the prompt 
+    try:
+        subprocess.run(prompt,check=True)
+    except subprocess.CalledProcessError as e: # handle error with return code (check = True)
+        print(f"The prompt failed with the return code: {e.returncode}")
+
+#run_jplag("C:/Users/claud/OneDrive/Ambiente de Trabalho/Projeto Final/jplagsegundogithub/jplag.jar",
+#          "./data/Ficha03_041",
+#          "./output/resultado")
+
+
+#java -jar jplag.jar -l java ./Testes/Testes/Ficha03_041 -r ./resultados/resultado
