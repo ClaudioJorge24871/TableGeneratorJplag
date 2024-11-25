d241b068698dc8d24dbe993500333cad35d13319
owanedee <claudiojorge9090@gmail.com>
2024-11-25 15:31:37 +0100

    Formated Runner and Parser

--- utils/jplag_runner.py
+++ utils/jplag_runner.py
@@ -3,12 +3,12 @@ import subprocess
 def run_jplag(jplag_path, dir_source, dir_output):
     """
     Runs jplag 
-    given the path of jplag, the source/data and the directory of output 
+     given the path of jplag, the source/data and the directory of output 
     
     PARAMS:
-    jplag_path - path to the jplag jar executable
-    dir_source - directory to the code submissions
-    dir_output - directory to the code results  
+        jplag_path - path to the jplag jar executable
+        dir_source - directory to the code submissions
+        dir_output - directory to the code results  
     """
 
     # Language required by jplag, must be the language of the submitted codes 
@@ -25,5 +25,5 @@ def run_jplag(jplag_path, dir_source, dir_output):
     # Try-Catch to run the prompt 
     try:
         subprocess.run(prompt,check=True)
-    except subprocess.CalledProcessError as e: # handle error with return code (check = True)
+    except subprocess.CalledProcessError as e: # Handle error with return code (check = True)
         print(f"The prompt failed with the return code: {e.returncode}")
--- utils/result_parser.py
+++ utils/result_parser.py
@@ -2,49 +2,54 @@ import json
 import zipfile
 import os
 
-#path to zip file
-zip_file_path = "./output/results.zip"
-output_directory = "./output"
+def parse_results(zip_file_path,output_directory):
+    """
+    Creates a JSON file ease to transform later in table 
+      given the path to the results file 
+    
+    PARAMS:
+        zip_file_path - path to the zip file results created by JPLAG 
+        output_directory - path to the output folder
+    """
 
-# Open and extract all files to the specified output_directory
-with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    # Open and extract all files to the specified output_directory
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
 
-    # List all files of the output_directory that end with ".json"
-    json_files = [f for f in zip_ref.namelist() if f.endswith('.json')]
+        # List all files of the output_directory that end with ".json"
+        json_files = [f for f in zip_ref.namelist() if f.endswith('.json')]
 
-    for json_file in json_files:
-        zip_ref.extract(json_file, output_directory)
+        for json_file in json_files:
+            zip_ref.extract(json_file, output_directory)
 
-    print(f"Files extracted to {output_directory}")
+        print(f"Files extracted to {output_directory}")
 
-#stores the extrated data
-extracted_data = []
+    # Stores the extrated data
+    extracted_data = []
 
-# Iterate extrated files 
-for file_name in json_files:
-    file_path = os.path.join(output_directory, file_name)  # Creates the full path to file
-   
-    #ignores the files options, overview and submissionFileIndex
-    if file_name not in {"options.json", "overview.json", "submissionFileIndex.json"}: 
-        try:
-            with open(file_path, 'r', encoding='utf-8') as f:
-                data = json.load(f)
-                id1 = data["id1"]
-                id2 = data["id2"]
-                max_value = data.get('similarities').get('MAX', '')
+    # Iterate extrated files 
+    for file_name in json_files:
+        file_path = os.path.join(output_directory, file_name)  # Creates the full path to file
+    
+        # Ignores the files options, overview and submissionFileIndex
+        if file_name not in {"options.json", "overview.json", "submissionFileIndex.json"}: 
+            try:
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    data = json.load(f)
+                    id1 = data["id1"]
+                    id2 = data["id2"]
+                    max_value = data.get('similarities').get('MAX', '')
+                        
+                    # Appends to the extracted_data the json of the current comparison
+                    extracted_data.append({'id1':id1, 'id2':id2, 'MAX':max_value})
                     
-                extracted_data.append({'id1':id1, 'id2':id2, 'MAX':max_value})
-                
-            """
-            Algoritmo 
-            retirar de data os parametros: id1, id2 e MAX
-            adicionar ao ficheiro json criado anteriormente os valores dos parametros 
-            """
-        except Exception as e:
-            print(f"Erro ao processar {file_name}: {e}")
+            except Exception as e:
+                print(f"Error processing {file_name}: {e}")
 
-    os.remove(file_path)
+        # Clears the files on output directory
+        os.remove(file_path)
 
-with open("./output/mydata.json", "w") as final:
-	json.dump(extracted_data, final)
-     
+    # Creates a file named mydata.json
+    with open("./output/mydata.json", "w") as final:
+        # Serializes obj to JSON 
+        json.dump(extracted_data, final)
+        
